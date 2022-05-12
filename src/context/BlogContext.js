9fe7728211edb6ff97617f6125a9fdcc6053c45c
jember08 <afandayul@gmail.com>
2022-05-13 02:55:39 +0700

    moving data with context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,5 +3,11 @@ import React from "react";
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider>{children}</BlogContext.Provider>;
+  return (
+    <BlogContext.Provider value="Moving data with context">
+      {children}
+    </BlogContext.Provider>
+  );
 };
+
+export default BlogContext;
--- src/screens/IndexScreens.js
+++ src/screens/IndexScreens.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import BlogContext from "../context/BlogContext";
 
 const IndexScreens = () => {
+  const value = useContext(BlogContext);
+
   return (
     <View>
       <Text>Index Screens</Text>
+      <Text>{value}</Text>
     </View>
   );
 };
