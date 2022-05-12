842529776d938ee1a479d4a6006305b98b53e878
jember08 <afandayul@gmail.com>
2022-05-13 02:44:22 +0700

    adding context

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import IndexScreens from "./src/screens/IndexScreens";
+import { BlogProvider } from "./src/context/BlogContext";
 
 const navigator = createStackNavigator(
   {
@@ -17,5 +18,9 @@ const navigator = createStackNavigator(
 const App = createAppContainer(navigator);
 
 export default () => {
-  return <App />;
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
 };
