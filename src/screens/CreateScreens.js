f3ac55004659a444c70b65451275ae9166f2f15e
Afan Dayu Laksono <afandayul@gmail.com>
2022-05-28 03:55:39 +0700

    displaying a form

--- src/screens/CreateScreens.js
+++ src/screens/CreateScreens.js
@@ -1,15 +1,43 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useContext, useState } from "react";
+import { Text, TextInput, View, StyleSheet, Button } from "react-native";
 import { Context } from "../context/BlogContext";
 
-const CreateScreens = () => {
+const CreateScreens = ({ navigation }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
   return (
     <View>
-      <Text>Create Screen</Text>
+      <Text style={styles.label}>Enter Title:</Text>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+      />
+      <Text style={styles.label}>Enter Content:</Text>
+      <TextInput
+        style={styles.input}
+        value={content}
+        onChangeText={(text) => setContent(text)}
+      />
+      <Button title="Add blog" />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    marginHorizontal: 5,
+    marginBottom: 15,
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 10,
+    marginHorizontal: 5,
+  },
+});
 
 export default CreateScreens;
