fb2b8735cb09a7828308f744e72b75443a8d143a
Afan Dayu Laksono <afandayul@gmail.com>
2022-05-28 03:16:44 +0700

    adding a creation screen

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import { createStackNavigator } from "react-navigation-stack";
 import IndexScreens from "./src/screens/IndexScreens";
 import { Provider } from "./src/context/BlogContext";
 import ShowScreens from "./src/screens/ShowScreens";
+import CreateScreens from "./src/screens/CreateScreens";
 
 const navigator = createStackNavigator(
   {
     Index: IndexScreens,
     Show: ShowScreens,
+    Create: CreateScreens,
   },
   {
     initialRouteName: "Index",
---
+++ src/screens/CreateScreens.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Context } from "../context/BlogContext";
+
+const CreateScreens = () => {
+  return (
+    <View>
+      <Text>Create Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CreateScreens;
