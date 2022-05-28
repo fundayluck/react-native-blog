1b93c7b87d9bd77e49927fa533b697e1807c3c02
Afan Dayu Laksono <afandayul@gmail.com>
2022-05-28 16:19:26 +0700

    initializing state from context

--- src/screens/EditScreens.js
+++ src/screens/EditScreens.js
@@ -1,23 +1,30 @@
 import React, { useContext, useState } from "react";
 import { View, Text, TextInput, StyleSheet, Button } from "react-native";
+import { Context } from "../context/BlogContext";
 
 const EditScreens = ({ navigation }) => {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+  const { state } = useContext(Context);
+
+  const blogPost = state.find(
+    (blogPost) => blogPost.id === navigation.getParam("id")
+  );
+
+  const [title, setTitle] = useState(blogPost.title);
+  const [content, setContent] = useState(blogPost.content);
   return (
     <View>
       <Text>Edit Screen - {navigation.getParam("id")}</Text>
-      <Text style={styles.label}>Enter Title:</Text>
+      <Text style={styles.label}>Edit Title:</Text>
       <TextInput
         style={styles.input}
         value={title}
-        onChangeText={(text) => setTitle(text)}
+        onChangeText={(newTitle) => setTitle(newTitle)}
       />
       <Text style={styles.label}>Enter Content:</Text>
       <TextInput
         style={styles.input}
         value={content}
-        onChangeText={(text) => setContent(text)}
+        onChangeText={(newContent) => setContent(newContent)}
       />
       <Button
         onPress={() => {
