c06ee98c8de1f502d630b8816eb9edf83e329fd4
Afan Dayu Laksono <afandayul@gmail.com>
2022-05-28 16:57:00 +0700

    initial form values

--- src/screens/EditScreens.js
+++ src/screens/EditScreens.js
@@ -10,7 +10,14 @@ const EditScreens = ({ navigation }) => {
     (blogPost) => blogPost.id === navigation.getParam("id")
   );
 
-  return <BlogPostForm />;
+  return (
+    <BlogPostForm
+      initialValues={{ title: blogPost.title, content: blogPost.content }}
+      onSubmit={(title, content) => {
+        console.log(title, content);
+      }}
+    />
+  );
 };
 
 const styles = StyleSheet.create({});
